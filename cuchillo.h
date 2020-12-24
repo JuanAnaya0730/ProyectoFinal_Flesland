d807465022452d9889ff5c509827eaa5be4bceb0
Juan Anaya <juan.anayar@udea.edu.co>
2020-12-23 19:56:30 -0500

    Se agrego la clase "Cuchillo" que se encargara de trabajar en conjunto con la clase personaje ya que estos seran los proyectiles que el personaje principal lanzara.

--- cuchillo.h
+++ cuchillo.h
@@ -1,11 +1,36 @@
+/*! La clase cuchillo se encargara de trabajar en conjunto con la clase personaje ya que estos
+ *  seran los proyectiles que el personaje principal lanzara.
+ *  Estos cuchillos o proyectiles describiran un movimiento rectilineo uniforme(MRU).
+*/
+
 #ifndef CUCHILLO_H
 #define CUCHILLO_H
 
+#include <QGraphicsObject>
+#include <QObject>
+#include <QGraphicsRectItem>
+#include <QTimer>
 
-class Cuchillo
+#define V 2 // Velocidad a la que se mueve
+
+class Cuchillo:public QObject, public QGraphicsRectItem
 {
+private:
+    int posicionX; // Posicion en X despues de un determinado tiempo.
+    int posicionY; // Posicion en Y. constante.
+    int X0; // Posicion inicial.
+    int t; // Intervalo de tiempo durante el cual se mueve el cuerpo.
+    QTimer *timer;
+
 public:
-    Cuchillo();
+    Cuchillo(int posicionInicialX, int posicionconstanteY);
+
+    int getPosicionX() const;
+    int getPosicionY() const;
+
+public slots:
+    void actulizarposicion(void);
+
 };
 
 #endif // CUCHILLO_H
