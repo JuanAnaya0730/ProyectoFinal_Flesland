256ca839987f8463ca33e934abf6dff4bdb08713
lizeth.villa <lizeth.villa@udea.edu.co>
2020-12-27 16:42:23 -0500

    .

--- plataformas.cpp
+++ plataformas.cpp
@@ -1,6 +1,20 @@
 #include "plataformas.h"
 
-plataformas::plataformas()
+plataformas::plataformas(int x, int y, int w_, int h_)
 {
+    posx = x;
+    posy = y;
+    w = w_;
+    h = h_;
+}
 
+QRectF plataformas::boundingRect() const
+{
+   return QRectF(posx,posy,w,h);
+}
+
+void plataformas::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    painter->setBrush(Qt::blue);//     transparent
+    painter->drawRect(boundingRect());
 }
